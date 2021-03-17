eb5de3ac961e5db8ccc98f64c0de2b0e5bff2b14
doriensimon <dksimon@stanford.edu>
2021-03-17 10:36:43 -0600

    unit tracking for major requirements

--- req_builder.py
+++ req_builder.py
@@ -3,9 +3,8 @@ Define a req as taking NUM out of the reqs specified in list REQS.
 A req is a list of courses or reqs.
 '''
 class Req():
-    def __init__(self, reqs, units, num=-1):
+    def __init__(self, reqs, num=-1):
         self.reqs = reqs
         self.num = num
-        self.units = units
         self.all = num == -1 #I don't understand what this means
         
